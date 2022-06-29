6b12531a61c36b634d1e42362d7346aeb20644e5
Usman Adil <usmanadil431@gmail.com>
2022-06-29 16:21:50 +0500

    Pdf and docusign first commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DocusignController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('docusign',[DocusignController::class, 'index'])->name('docusign');
+Route::get('connect-docusign',[DocusignController::class, 'connectDocusign'])->name('connect.docusign');
+Route::get('docusign/callback',[DocusignController::class,'callback'])->name('docusign.callback');
+Route::get('sign-document',[DocusignController::class,'signDocument'])->name('docusign.sign');
+Route::get('test',[DocusignController::class,'test'])->name('test');
